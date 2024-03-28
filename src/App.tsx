57c5a25d853e567a12405b8a05e4e85584f3eff6
HamidGX <hamid.uxw@gmail.com>
2024-03-28 03:45:15 -0600

    feat: add provider and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { ConfirmAccount } from '@/pages/ConfirmAccount'
+import { CreateAccount } from '@/pages/CreateAccount'
+import { ForgotPassword } from '@/pages/ForgotPassword'
+import Home from '@/pages/Home'
+import { Login } from '@/pages/Login'
+import { NewPassword } from '@/pages/NewPassword'
+import { Register } from '@/pages/Register'
+
+import AuthLayout from '@/layouts/AuthLayout'
+import ProtectedRoute from '@/layouts/ProtectedRoute'
+
+import { AuthProvider } from '@/context/AuthProvider'
+
 export default function App() {
-	return <div>App</div>
+	return (
+		<BrowserRouter>
+			<AuthProvider>
+				<Routes>
+					<Route path="/" element={<AuthLayout />}>
+						<Route index element={<Login />} />
+						<Route path="register" element={<Register />} />
+						<Route path="create-account" element={<CreateAccount />} />
+						<Route path="forgot-password" element={<ForgotPassword />} />
+						<Route path="forgot-password/:id" element={<NewPassword />} />
+						<Route path="confirm/:id" element={<ConfirmAccount />} />
+					</Route>
+					<Route path="/home" element={<ProtectedRoute />}>
+						<Route index element={<Home />} />
+					</Route>
+				</Routes>
+			</AuthProvider>
+		</BrowserRouter>
+	)
 }
